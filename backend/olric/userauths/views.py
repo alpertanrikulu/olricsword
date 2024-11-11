4d73f852c73c2155fb4f57eca456135447d43358
alpertanrikulu <tanrikulualper77100@gmail.com>
2024-11-11 17:40:17 +0300

    user token creation apis created

--- backend/olric/userauths/views.py
+++ backend/olric/userauths/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from userauths.models import Profile, User
+from userauths.serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+import shortuuid
+import random
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = RegisterSerializer
+
+def generate_otp():
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:6]
+    return unique_key
+
+class PasswordResetEmailVerify(generics.RetrieveAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects.get(email=email)
+
+
+
+        if user:
+            user.otp = generate_otp()
+            user.save()
+
+            uidb64 = user.pk
+            otp = user.otp
+
+            link = f"http://localhost:5173/create-new-password?otp={otp}&uidb64={uidb64}"
+            print("##########\n", link)
+
+        return user
+
+class PasswordChangeView(generics.CreateAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        payload = request.data
+        otp = payload['otp']
+        uidb64 = payload['uidb64']
+        # reset_token = payload['reset_token']
+        password = payload['password']
+
+        user = User.objects.get(id=uidb64, otp=otp)
+        if user:
+            user.set_password(password)
+            user.otp = ""
+            user.reset_token = ""
+            user.save()
+
+            
+            return Response( {"message": "Password Changed Successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response( {"message": "An Error Occured"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
